0d88d6dacb248e8f803fccceba0e2f9bd5865607
Allen <allen.davisswing@gmail.com>
2024-06-28 23:41:24 -0400

    repl

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,16 @@ impl Command {
 }
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    
 
     // Define the commands
 
+    // Empty command
+    let empty_command = Command::new("", |_| {});
+
     // Default command not found
     let command_not_found = Command::new("command_not_found", |args| {
-        println!("{}: command not found", args[0]);
+        println!("{}: command not found", args.get(0).unwrap_or(&String::from("error")));
     });
 
     let mut commands: HashMap<&str, Command> = HashMap::new();
@@ -40,20 +41,26 @@ fn main() {
     }));
 
     commands.insert(&command_not_found.name, command_not_found.clone());
+    commands.insert(&empty_command.name, empty_command.clone());
 
+    loop {
+        //  Print the prompt
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
-    // Parse the input
-    let parts: Vec<String> = input.trim().split_whitespace().map(|str| { str.to_string()}).collect();
-    let command = parts.get(0).unwrap();
-
-    // Find the command
-    let handler = commands.get(command.as_str()).unwrap_or(&command_not_found);
-
-    // Run the command
-    handler.run(&parts);
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+    
+        // Parse the input
+        let parts: Vec<String> = input.trim().split_whitespace().map(|str| { str.to_string()}).collect();
+        let command = parts.get(0).unwrap_or(&String::from("")).clone();
+    
+        // Find the command
+        let handler = commands.get(command.as_str()).unwrap_or(&command_not_found);
+    
+        // Run the command
+        handler.run(&parts);
+    }
 }
