4d333f14a6150429d29a92a7405729d8898a18d5
Allen <allen.davisswing@gmail.com>
2024-07-05 21:25:27 -0400

    type command

--- src/main.rs
+++ src/main.rs
@@ -2,70 +2,114 @@ use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+struct Shell {
+    commands: HashMap<String, Handler>,
+}
+
+impl Shell {
+    fn new() -> Shell {
+        Shell {
+            commands: HashMap::new(),
+        }
+    }
+
+    fn add_command(&mut self, command: Command) {
+        self.commands.insert(command.name, command.handler);
+    }
+
+    fn run(&self) {
+
+        let not_found = Command::new("command_not_found", |args, _| {
+            println!("{}: command not found", args.get(0).unwrap_or(&String::from("error")));
+        });
+
+        loop {
+            //  Print the prompt
+            print!("$ ");
+            io::stdout().flush().unwrap();
+
+            // Wait for user input
+            let stdin = io::stdin();
+            let mut input = String::new();
+            stdin.read_line(&mut input).unwrap();
+        
+            // Parse the input
+            let parts: Vec<String> = input.trim().split_whitespace().map(|str| { str.to_string()}).collect();
+            let command = parts.get(0);
+
+            if command.is_none() {
+                (not_found.handler)(&parts, &self);
+                continue;
+            }
+
+            let command = command.unwrap();
+
+            // Find the command
+            let handler = self.commands.get(command.as_str());
+
+            // If the command is not found, run the default command_not_found
+            if handler.is_none() {
+                (not_found.handler)(&parts, &self);
+                continue;
+            }
+
+            let handler = handler.unwrap();
+
+            // Run the command
+            handler(&parts, &self);
+        }
+    }
+
+}
+
+type Handler = fn(&Vec<String>, &Shell);
+
 #[derive(Debug, Clone)]
 struct Command {
     name: String,
-    handler: fn(&Vec<String>),
+    handler: Handler,
 }
 
 impl Command {
-    fn new(name: &str, handler: fn(&Vec<String>)) -> Command {
+    fn new(name: &str, handler: Handler) -> Command {
         Command {
             name: name.to_string(),
             handler,
         }
     }
-
-    fn run(&self, args: &Vec<String>) {
-        (self.handler)(args);
-    }
 }
 
 fn main() {
     // Define the commands
+    let mut shell = Shell::new();
 
     // Empty command
-    let empty_command = Command::new("", |_| {
-    });
+    shell.add_command(Command::new("", |_, _| {}));
 
-    // Default command not found
-    let command_not_found = Command::new("command_not_found", |args| {
-        println!("{}: command not found", args.get(0).unwrap_or(&String::from("error")));
-    });
-
-    let mut commands: HashMap<&str, Command> = HashMap::new();
-
-    commands.insert("echo", Command::new("echo", |args| {
+    shell.add_command(Command::new("echo", |args, _| {
         let rest = args.iter().skip(1).fold(String::new(), |acc, arg| acc + " " + arg);
         println!("{}", rest.trim());
     }));
 
-    commands.insert("exit", Command::new("exit", |args| {
+    shell.add_command(Command::new("exit", |args, _| {
         let status = args.get(1).unwrap_or(&String::from("0")).parse::<i32>().unwrap_or(0);
         std::process::exit(status);
     }));
 
-    commands.insert(&command_not_found.name, command_not_found.clone());
-    commands.insert(&empty_command.name, empty_command.clone());
+    shell.add_command(Command::new("type", |args, shell| {
+        let command = args.get(1);
+        if command.is_none() {
+            println!("type: missing argument");
+            return;
+        }
+        let command = command.unwrap();
+        if shell.commands.contains_key(command.as_str()) {
+            println!("{} is a shell builtin", command);
+            return;
+        }
+        println!("{}: not found", command);
+    }));
 
-    loop {
-        //  Print the prompt
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        // Wait for user input
-        let stdin = io::stdin();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-    
-        // Parse the input
-        let parts: Vec<String> = input.trim().split_whitespace().map(|str| { str.to_string()}).collect();
-        let command = parts.get(0).unwrap_or(&String::from("")).clone();
-    
-        // Find the command
-        let handler = commands.get(command.as_str()).unwrap_or(&command_not_found);
-    
-        // Run the command
-        handler.run(&parts);
-    }
+    // Run the shell
+    shell.run();
 }
